2a72413cdf9233e6bb78a90fb1c3d1a4cf4ab642
KarinaLaroze <karinalaroze@gmail.com>
2017-05-09 11:40:47 -0300

    recien comenzando

---
+++ main.js
@@ -0,0 +1,12 @@
+function intersection(l1, l2){
+	 [var l3= [];
+	 for(var j=0; i<l1.length; i++){
+	 	for(var j=0; j<l2.length;j++){
+	 		if(l1[i]==l2[j]){
+	 			l3.push(l1[i]);
+	 		}
+	 	}
+	 }
+	 return l3;
+}
+
