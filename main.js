80e92db80d60c2c848d1a410dc38ad3f581dbb85
KarinaLaroze <karinalaroze@gmail.com>
2017-05-09 11:45:50 -0300

    ejercicio 1 terminado

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function intersection(l1, l2){
-	 [var l3= [];
-	 for(var j=0; i<l1.length; i++){
+	 var l3= [];
+	 for(var i=0; i<l1.length; i++){
 	 	for(var j=0; j<l2.length;j++){
 	 		if(l1[i]==l2[j]){
 	 			l3.push(l1[i]);
 	 		}
 	 	}
 	 }
-	 return l3;
+	return l3;
 }
 
+console.log(intersection([1,2,3],[3,4,5]));
